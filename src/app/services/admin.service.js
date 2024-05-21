54c499ee9c528005c14d65b968808c9c5a3f6a9b
spykelionel <social.lionel@gmail.com>
2024-05-21 14:43:04 +0100

    add tag types

--- src/app/services/admin.service.js
+++ src/app/services/admin.service.js
@@ -13,24 +13,34 @@ export const adminApi = createApi({
       }
     },
   }),
+  tagTypes: ["Member"],
   endpoints: (builder) => ({
     createMember: builder.mutation({
       query: (body) => ({ body, url: "create", method: "POST" }),
+      invalidatesTags: [{ type: "Member", id: "LIST" }],
+      providesTags: [{ type: "Member", id: "LIST" }],
     }),
     deleteMember: builder.mutation({
       query: (id) => ({ url: `delete/${id}`, method: "DELETE" }),
+      invalidatesTags: [{ type: "Member", id: "LIST" }],
+      providesTags: [{ type: "Member", id: "LIST" }],
     }),
     getSingleMember: builder.query({
       query: (id) => ({ url: `${id}`, method: "GET" }),
     }),
     getAllMembers: builder.query({
       query: () => ({ url: ``, method: "GET" }),
+      providesTags: [{ type: "Member", id: "LIST" }],
     }),
     getStatistics: builder.query({
       query: () => ({ url: `stats`, method: "GET" }),
+      invalidatesTags: [{ type: "Member", id: "LIST" }],
+      providesTags: [{ type: "Member", id: "LIST" }],
     }),
     updateMember: builder.mutation({
       query: ({ id, body }) => ({ url: `update/${id}`, method: "PUT", body }),
+      invalidatesTags: [{ type: "Member", id: "LIST" }],
+      providesTags: [{ type: "Member", id: "LIST" }],
     }),
   }),
 });
