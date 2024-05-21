3904a42db2a2145e8dcdac744a34581774e829a9
spykelionel <social.lionel@gmail.com>
2024-05-21 14:16:41 +0100

    add update form

--- src/pages/admin/components/partials/MemberRow.jsx
+++ src/pages/admin/components/partials/MemberRow.jsx
@@ -1,3 +1,8 @@
+import { ModalWrapper } from "@components";
+import { Button, Spinner } from "@components/atoms";
+import { useState } from "react";
+import { useUpdateMemberMutation } from "../../../../app/services/admin.service";
+
 export function MemberRow({
   idx,
   name,
@@ -8,6 +13,39 @@ export function MemberRow({
   father,
   mother,
 }) {
+  const init = {
+    name,
+    email,
+    dateOfBirth,
+    placeOfResidence,
+    phoneNumber,
+    father,
+    mother,
+  };
+  const [form, setForm] = useState(init);
+
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const [openSuccessModal, setOpenSuccessModal] = useState(false);
+
+  const [updateMember, { isLoading }] =
+    useUpdateMemberMutation("update-member");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await updateMember(form).unwrap();
+      if (res) {
+        setOpenSuccessModal(true);
+      }
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
   return (
     <tr className="bg-white border-b  hover:bg-gray-50 ">
       <td className="px-6 py-2">{idx}</td>
@@ -23,11 +61,159 @@ export function MemberRow({
       <td className="px-6 py-2">{phoneNumber ?? "None"}</td>
       <td className="px-6 py-2">{mother?.name ?? "None"}</td>
       <td className="px-6 py-2">{father?.name ?? "None"}</td>
-      <td className="px-6 py-2 text-right">
-        <a href="#" className="font-medium text-blue-600 hover:underline">
-          Edit
-        </a>
+      <td className="px-6 py-2 flex flex-row gap-2">
+        <Button
+          title={"Edit"}
+          type={"primary"}
+          onClick={() => setOpenEditModal(true)}
+        />
+        <Button title={"Delete"} type={"gray"} />
       </td>
+      {openEditModal && (
+        <ModalWrapper
+          openModal={openEditModal}
+          closeModal={() => setOpenEditModal(false)}
+          className="h-screen"
+        >
+          <div className="grid place-items-center">
+            <form
+              className="w-full h-full mx-auto shadow-md p-5"
+              onSubmit={onSubmit}
+            >
+              <p className="py-2 font-bold text-lg text-center">
+                Add a member to your <br />
+                <span className="text-blue-500">Family tree</span>
+              </p>
+              <div className="mb-5">
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Member Name
+                </label>
+                <input
+                  type="name"
+                  id="name"
+                  name="name"
+                  value={form.name}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5"
+                  placeholder="member name"
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Member email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5"
+                  placeholder="name@family.com"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="dateOfBirth"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Date of Birth
+                </label>
+                <input
+                  type="date"
+                  id="dateOfBirth"
+                  name="dateOfBirth"
+                  value={form.dateOfBirth}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5"
+                  placeholder="Date of Birth"
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label
+                  htmlFor="placeOfResidence"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Place of Residence
+                </label>
+                <input
+                  type="text"
+                  id="placeOfResidence"
+                  name="placeOfResidence"
+                  value={form.placeOfResidence}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5"
+                  placeholder="Place of placeOfResidence"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="phoneNumber"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  phone number
+                </label>
+                <input
+                  type="text"
+                  id="phoneNumber"
+                  name="phoneNumber"
+                  value={form.phoneNumber}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5"
+                  placeholder="phoneNumber"
+                  required
+                />
+              </div>
+
+              <div className="w-full">
+                {isLoading ? (
+                  <Spinner />
+                ) : (
+                  <button
+                    type="submit"
+                    className="text-white bg-blue-500 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center  "
+                  >
+                    Update member
+                  </button>
+                )}
+              </div>
+            </form>
+          </div>
+          {openSuccessModal && (
+            <ModalWrapper
+              openModal={openSuccessModal}
+              closeModal={() => setOpenSuccessModal(false)}
+            >
+              <div className="flex flex-col items-center justify-center">
+                <SuccessSvg />
+                <p>Member has been updated successfully. </p>
+                <div className="flex flex-row justify-between items-center gap-4">
+                  <Button
+                    title={"Return"}
+                    type={"gray"}
+                    onClick={() => {
+                      setOpenSuccessModal(false);
+                      setOpenEditModal(false);
+                    }}
+                  ></Button>
+                </div>
+              </div>
+            </ModalWrapper>
+          )}
+        </ModalWrapper>
+      )}
     </tr>
   );
 }
